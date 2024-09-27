b13600aa4c13806775be48dd41518e7671b86f9d
Dayne Broderson <broderson@gmail.com>
2024-09-26 22:32:16 -0800

    capsfix-hyprland.sh sanity

--- scripts/capsfix-hyprland.sh
+++ scripts/capsfix-hyprland.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/bash
-
-# TODO verify hyprland context
-# ensure we are in hyprland
-
 config_file="$HOME/.config/hypr/UserConfigs/UserSettings.conf"
 xkb_options="caps:ctrl_modifier"
 
-#sed -i 's/Prompt=lts/Prompt=normal/' "$config_file"
-#target_line="  kb_options ="
+# verify hyprland context
+if ! command-v hyprland; then
+    echo "hyprland not available - no point in messing with this"
+    exit 1
+fi
+
+# verify hyprland config_file setup
+if [[ ! -f $config_file ]]; then
+    echo "error: config_file not found $config_file"
+    exit 1
+fi
+
+grep "${xkb_options}" ${config_file} > /dev/null
+if [ $? -eq 0 ]; then
+    echo "success: kb_options=${xkb_options} already set in ${config_file}"
+    exit 0
+fi
+
 sed_string='s/  kb_options =/  kb_options = '${xkb_options}'/'
 echo "sed -i '${sed_string}' $config_file"
 sed -i "${sed_string}" "$config_file"
